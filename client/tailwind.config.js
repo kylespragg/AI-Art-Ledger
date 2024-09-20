dbce7e1e162e71c4d427995c3f6d0c95d7423f2e
Kyle Spragg <kylesspragg@gmail.com>
2024-09-20 16:43:54 -0500

    Updated file structure and additional frameworks

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/Login.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
